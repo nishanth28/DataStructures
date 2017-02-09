fec6d9ff54174692a153f2b08b1fe30398b0eaf2
nishanth28 <pnishanth28@gmail.com>
2017-02-09 18:50:56 -0500

    Completed: Sorting Algorithms

--- Sorting.py
+++ Sorting.py
@@ -23,6 +23,8 @@ def BubbleSort(arr):
                     arr[j],arr[j-1] = arr[j-1],arr[j]
         return arr
 
+#-------------------------------------------------------------------------#
+
 def SelectionSort(arr):
 
     #improves on the bubble sort
@@ -41,15 +43,163 @@ def SelectionSort(arr):
         arr[MaxPosition] = temp
 
     return arr
-#def quick_sort(arr):
 
-#def merge_sort(arr):
+#-------------------------------------------------------------------------#
+
+def insertionSort(arr):
+
+    # n-1 passes to sort n items
+    # order(n2)
+
+    for i in range(1,len(arr)):
+
+        current = arr[i]
+        position = i
+
+        while position > 0 and arr[position-1] > current:
+            arr[position] = arr[position-1]
+            position = position-1
+
+        arr[position] = current
+
+
+#-------------------------------------------------------------------------#
+
+
+def shellSort(arr):
+
+    # Shell-Sort improves on insertion sort by breaking the original list into a number of smaller sublists,
+    # Uses i (gap) apart!
+    # reduces the number of shifting operation of insertion sort
+    # does small sort and then finally a insertion sort pass.
+    sublistCount = len(arr)/2
+
+    while sublistCount > 0:
+        for start in range(sublistCount):
+            gap_insertion_sort(arr,start,sublistCount)
+            print(arr)
+        sublistCount = sublistCount/2
+
+
+def gap_insertion_sort(arr,start,gap):
+
+    for i in range(start+gap,len(arr),gap):
+        currentvalue = arr[i]
+        position = i
+
+        while position >= gap and arr[position-gap] > currentvalue:
+            arr[position]= arr[position-gap]
+            position = position - gap
+
+        arr[position] = currentvalue
+
+
+#-------------------------------------------------------------------------#
+
+def quickSort(arr):
+
+    # Quicksort select a value called pivot value
+    # pivot assist with splitting the list and call recursively
+    # does not use additional space
+
+    return quickHelp(arr,0,len(arr)-1)
+
+
+
+def quickHelp(arr,first,last):
+
+    if first < last:
+
+        splitPoint = partition(arr,first,last)
+
+        quickHelp(arr,first,splitPoint-1)
+        quickHelp(arr, splitPoint+1, last)
+
+    return arr
+
+def partition(arr,first,last):
+
+    #returns the split point
+
+    pivot = arr[first]
+
+    leftmark = first + 1
+    rightmark = last
+
+    done = False
+
+    while not done:
+
+        while leftmark <= rightmark and arr[leftmark] <= pivot:
+            leftmark +=1
+        while rightmark >= leftmark and arr[rightmark] >= pivot:
+            rightmark -=1
+
+        if rightmark < leftmark:
+            done = True
+
+        else:
+            temp = arr[leftmark]
+            arr[leftmark] = arr[rightmark]
+            arr[rightmark] = temp
+
+    temp = arr[first]
+    arr[first] = arr[rightmark]
+    arr[rightmark] = temp
+
+    return rightmark
+
+#-----------------------------------------------------------------------------#
+def mergeSort(arr):
+
+    # Merge Sort is a recursive algo that continually split a list in half.
+    # split and recursively apply mergesort
+    # atlast merge the sorted list.
+
+    #while merge . if splits . Recursively
+    if len(arr) > 1:
+
+        mid = len(arr)/2
+        lefthalf = arr[:mid]
+        righthalf = arr[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(lefthalf) and j < len(righthalf):
+
+            if lefthalf[i] < righthalf [j]:
+                arr[k] = lefthalf[i]
+                i += 1
+
+            else:
+
+                arr[k] = righthalf[j]
+                j += 1
+
+            k += 1
+
+        while i < len(lefthalf):
+            arr[k] = lefthalf[i]
+            i += 1
+            k += 1
+
+        while j < len(righthalf):
+            arr[k] = righthalf[j]
+            j += 1
+            k += 1
+
+    return arr
+
 
 #def heap_sort(arr):
 
 
 arr = [1,367,8,90,899,53]
 print arr
-#print BubbleSort(arr)
-print SelectionSort(arr)
+print quickSort(arr)
 
